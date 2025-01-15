dd69991eebd636e7f1363e4fde5452402c1423e3
Dace Apsvalka <dace.apshvalka@gmail.com>
2025-01-15 11:48:36 +0000

    Iinclude 'rating' and 'filler' events and improve condition filtering

--- code/step08_fill_event_files.py
+++ code/step08_fill_event_files.py
@@ -1,3 +1,9 @@
+# -------------------------------
+# Dace Apsvalka, @CBU 2025
+# -------------------------------
+# This script reads the source Excel files and fills the BIDS events files with the relevant events.
+# It also creates 'rating' events for each trial (ratings occured 3 seconds after each T and NT event).
+
 from pathlib import Path
 import pandas as pd
 from bids import BIDSLayout
@@ -19,14 +25,15 @@ conditions = {
     'negNTni': {'condition_img': 'ENEG', 'trial_type': 's', 'intrusion': 0},
     'neutrT': {'condition_img': 'ENEU', 'trial_type': 'r', 'intrusion': 1},
     'neutrNTi': {'condition_img': 'ENEU', 'trial_type': 's', 'intrusion': 1},
-    'neutrNTni': {'condition_img': 'ENEU', 'trial_type': 's', 'intrusion': 0}
+    'neutrNTni': {'condition_img': 'ENEU', 'trial_type': 's', 'intrusion': 0},
+    'filler': {'condition_img': 'FN', 'trial_type': ['sF', 'rF'], 'intrusion': [1, 0]}
 }
 
 # Header for the source event file and the new output file
 source_header = ['trial', 'cue_img', 'condition_img', 'trial_type', 'intrusion_rating', 
                  'intrusion', 'RT', 'onset', 'duration', 'run']
 
-new_header = ['onset', 'duration', 'trial_type', 'response_time', 'stim_file']
+new_header = ['onset', 'duration', 'trial_type', 'stim_file']
 
 # Process each BIDS events file
 for events_file_path in events_files:
@@ -63,26 +70,38 @@ for events_file_path in events_files:
         'onset': pd.Series(dtype='float64'),
         'duration': pd.Series(dtype='float64'),
         'trial_type': pd.Series(dtype='object'),
-        'response_time': pd.Series(dtype='float64'),
         'stim_file': pd.Series(dtype='object')
     })
 
     # Process each condition
     for condition_name, condition_criteria in conditions.items():
+        trial_type_criteria = condition_criteria['trial_type']
+        intrusion_criteria = condition_criteria['intrusion']
+
+        # Handle cases where 'trial_type' or 'intrusion' is a list (like in 'filler')
+        if isinstance(trial_type_criteria, list):
+            trial_type_filter = events_for_run['trial_type'].isin(trial_type_criteria)
+        else:
+            trial_type_filter = events_for_run['trial_type'].str.contains(trial_type_criteria, na=False)
+
+        if isinstance(intrusion_criteria, list):
+            intrusion_filter = events_for_run['intrusion'].isin(intrusion_criteria)
+        else:
+            intrusion_filter = events_for_run['intrusion'] == intrusion_criteria
+
         # Filter data for the current run and condition
         filtered_events = events_for_run[
             (events_for_run['condition_img'].str.contains(condition_criteria['condition_img'], na=False)) & 
-            (events_for_run['trial_type'].str.contains(condition_criteria['trial_type'], na=False)) & 
-            (events_for_run['intrusion'] == condition_criteria['intrusion'])
+            trial_type_filter &
+            intrusion_filter
         ]
 
         if not filtered_events.empty:
             # Construct DataFrame for this condition
             new_data = pd.DataFrame({
                 'onset': filtered_events['onset'] / 1000,  # convert ms to seconds
-                'duration': 0, #filtered_events['duration'], # set duration to 0
+                'duration': filtered_events['duration'] / 1000,  # convert ms to seconds
                 'trial_type': condition_name,
-                'response_time': filtered_events['RT'],
                 'stim_file': filtered_events['condition_img']
             })
             
@@ -90,6 +109,20 @@ for events_file_path in events_files:
             new_data = new_data.reindex(columns=new_events.columns)  # Align columns
             if not new_data.empty and not new_data.isna().all(axis=None):
                 new_events = pd.concat([new_events, new_data], ignore_index=True)
+                
+            # Add 'rating' events
+            rating_data = pd.DataFrame({
+                'onset': (filtered_events['onset'] / 1000) + 3,  # Add 3 seconds to the onset
+                'duration': filtered_events['RT'],               # Duration is the RT
+                'trial_type': 'rating',
+                'stim_file': filtered_events['condition_img']
+            })
+            
+            # Concatenate 'rating' events with the main DataFrame
+            rating_data = rating_data.reindex(columns=new_events.columns)  # Align columns
+            if not rating_data.empty and not rating_data.isna().all(axis=None):
+                new_events = pd.concat([new_events, rating_data], ignore_index=True)
+
     
     # Sort the events by onset time
     new_events = new_events.sort_values(by='onset')
